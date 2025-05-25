cd618aa27c40961da84d2907184ee2dd131d804b
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-05-25 16:38:50 -0300

    first commit

---
+++ src/iPhone.java
@@ -0,0 +1,49 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    private String modelo;
+    private String numeroSerie;
+
+    public iPhone(String modelo, String numeroSerie) {
+        this.modelo = modelo;
+        this.numeroSerie = numeroSerie;
+    }
+
+    // Métodos do Reprodutor Musical
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando a música: " + musica);
+    }
+
+    // Métodos do Aparelho Telefônico
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero);
+    }
+
+    public void atender() {
+        System.out.println("Atendendo ligação...");
+    }
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Acessando correio de voz...");
+    }
+
+    // Métodos do Navegador na Internet
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+}
